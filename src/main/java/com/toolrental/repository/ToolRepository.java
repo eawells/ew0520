8cfda055c6788f9df8c8281fd3d59dcb5f635853
Elizabeth Wells <elizabeth.a.wells1@gmail.com>
2020-06-01 19:17:58 -0500

    Refactor to make data static

--- src/main/java/com/toolrental/repository/ToolRepository.java
+++ src/main/java/com/toolrental/repository/ToolRepository.java
@@ -7,11 +7,9 @@ import java.util.HashMap;
 
 public class ToolRepository {
     private HashMap<String, Tool> codeToToolMap;
-    private ToolRentalDataStore toolRentalDataStore;
 
     public ToolRepository(){
-        toolRentalDataStore = new ToolRentalDataStore();
-        codeToToolMap = toolRentalDataStore.getToolData();
+        codeToToolMap = ToolRentalDataStore.getToolData();
     }
 
     public Tool getTool(String code){
--- src/main/java/com/toolrental/utils/ToolRentalDataStore.java
+++ src/main/java/com/toolrental/utils/ToolRentalDataStore.java
@@ -7,8 +7,6 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 
 public class ToolRentalDataStore {
-    private HashMap<String, ToolType> typeToToolTypeMap;
-    private HashMap<String, Tool> codeToToolMap;
 
     private static final String LADDER_TYPE = "Ladder";
     private static final String CHAINSAW_TYPE = "Chainsaw";
@@ -19,28 +17,28 @@ public class ToolRentalDataStore {
     private static final String RIDGID_JACKHAMMER_CODE = "JAKR";
     private static final String DEWALT_JACKHAMMER_CODE = "JAKD";
 
-    public ToolRentalDataStore(){
-        setToolTypeData();
-        setToolData(typeToToolTypeMap);
+    private ToolRentalDataStore(){}
+
+    public static HashMap<String, Tool> getToolData(){
+        HashMap<String, ToolType> typeToToolTypeMap = createToolTypeData();
+        return createToolData(typeToToolTypeMap);
     }
 
-    public HashMap<String, Tool> getToolData(){
-        return codeToToolMap;
-    }
-
-    private void setToolTypeData(){
-        typeToToolTypeMap = new HashMap();
+    private static HashMap<String, ToolType> createToolTypeData(){
+        HashMap<String, ToolType> typeToToolTypeMap = new HashMap();
         typeToToolTypeMap.put(LADDER_TYPE, new ToolType(LADDER_TYPE, new BigDecimal("1.99"), true, true, false));
         typeToToolTypeMap.put(CHAINSAW_TYPE,  new ToolType(CHAINSAW_TYPE, new BigDecimal("1.49"), true, false, true));
         typeToToolTypeMap.put(JACKHAMMER_TYPE, new ToolType(JACKHAMMER_TYPE, new BigDecimal("2.99"), true, false, false));
+        return typeToToolTypeMap;
     }
 
-    private void setToolData(HashMap<String, ToolType> typeToToolTypeMap){
-        codeToToolMap = new HashMap();
+    private static HashMap<String, Tool> createToolData(HashMap<String, ToolType> typeToToolTypeMap){
+        HashMap<String, Tool> codeToToolMap = new HashMap();
         codeToToolMap.put(WERNER_LADDER_CODE, new Tool(WERNER_LADDER_CODE, typeToToolTypeMap.get(LADDER_TYPE), "Werner"));
         codeToToolMap.put(STIHL_CHAINSAW_CODE, new Tool(STIHL_CHAINSAW_CODE, typeToToolTypeMap.get(CHAINSAW_TYPE), "Stihl"));
         codeToToolMap.put(RIDGID_JACKHAMMER_CODE, new Tool(RIDGID_JACKHAMMER_CODE, typeToToolTypeMap.get(JACKHAMMER_TYPE), "Ridgid"));
         codeToToolMap.put(DEWALT_JACKHAMMER_CODE, new Tool(DEWALT_JACKHAMMER_CODE, typeToToolTypeMap.get(JACKHAMMER_TYPE), "DeWalt"));
+        return codeToToolMap;
     }
 
 }
